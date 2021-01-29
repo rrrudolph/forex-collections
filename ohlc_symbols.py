ce1d620593124488b59dfd6a04a9d0c50ff1193d
rrrudolph <rudysemail@gmail.com>
2021-01-28 22:35:19 -0600

    Updated symbols to reflect fxcm as data source

--- ohlc_symbols.py
+++ ohlc_symbols.py
@@ -1,51 +1,7 @@
-import MetaTrader5 as mt5
 
 
-mt5_timeframes = {
-                '1': mt5.TIMEFRAME_M1,
-                '5': mt5.TIMEFRAME_M5,
-                '15': mt5.TIMEFRAME_M15
-                }
-
-mt5_symbols = [
-    'EURUSD',
-    'GBPUSD',
-    'USDJPY',
-    'NZDUSD',
-    'USDCAD',
-    'AUDUSD',
-    'EURJPY',
-    'GBPJPY',
-    'CADJPY',
-    'AUDJPY',
-    'NZDJPY',
-    'EURCAD',
-    'EURAUD',
-    'GBPCAD',
-    'EURGBP',
-    'AUDCAD',
-    'NZDCAD',
-    'AUDNZD',
-    'GBPAUD',
-    'GBPNZD',
-    'EURNZD',
-    'USDCHF',
-    'GBPCHF',
-    'CADCHF',
-    'USDPLN',
-    'USDSEK',
-    'USDMXN',
-    'USDZAR',
-    'GBPSGD',
-    'USDZAR',
-    'XAUUSD',
-    'XAGUSD',
-    'XTIUSD',
-    'DE30',
-    'US30',
-    'US500',
-    'USTEC'
-]
+# This is the entire list of fxcm symbols, saved directly from stdout
+fx_symbols = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'EUR/CHF', 'AUD/USD', 'USD/CAD', 'NZD/USD', 'EUR/GBP', 'EUR/JPY', 'GBP/JPY', 'CHF/JPY', 'GBP/CHF', 'EUR/AUD', 'EUR/CAD', 'AUD/CAD', 'AUD/JPY', 'CAD/JPY', 'NZD/JPY', 'GBP/CAD', 'GBP/NZD', 'GBP/AUD', 'AUD/NZD', 'USD/SEK', 'EUR/SEK', 'EUR/NOK', 'USD/NOK', 'USD/MXN', 'AUD/CHF', 'EUR/NZD', 'USD/ZAR', 'USD/HKD', 'ZAR/JPY', 'USD/TRY', 'EUR/TRY', 'NZD/CHF', 'CAD/CHF', 'NZD/CAD', 'TRY/JPY', 'USD/ILS', 'USD/CNH', 'AUS200', 'ESP35', 'FRA40', 'GER30', 'HKG33', 'JPN225', 'NAS100', 'SPX500', 'UK100', 'US30', 'Copper', 'CHN50', 'EUSTX50', 'VOLX', 'USDOLLAR', 'US2000', 'INDIA50', 'USOil', 'UKOil', 'SOYF', 'NGAS', 'USOilSpot', 'UKOilSpot', 'WHEATF', 'CORNF', 'Bund', 'XAU/USD', 'XAG/USD', 'EMBasket', 'JPYBasket', 'BTC/USD', 'BCH/USD', 'ETH/USD', 'LTC/USD', 'XRP/USD', 'CryptoMajor', 'EOS/USD', 'XLM/USD', 'ESPORTS', 'BIOTECH', 'CANNABIS', 'FAANG', 'CHN.TECH', 'CHN.ECOMM', 'USEquities', 'AIRLINES', 'CASINOS', 'TRAVEL', 'US.ECOMM', 'US.BANKS', 'US.AUTO', 'WFH', 'BA.us', 'BAC.us', 'DIS.us', 'F.us', 'JPM.us', 'PFE.us', 'T.us', 'XOM.us', 'AAPL.us', 'AMZN.us', 'BIDU.us', 'FB.us', 'GOOG.us', 'INTC.us', 'MSFT.us', 'ACA.fr', 'AI.fr', 'BNP.fr', 'AIR.fr', 'FP.fr', 'ORA.fr', 'MC.fr', 'RNO.fr', 'SAN.fr', 'ADS.de', 'ALV.de', 'BAYN.de', 'BMW.de', 'DAI.de', 'DBK.de', 'DPW.de', 'DTE.de', 'LHA.de', 'SAP.de', 'AZN.uk', 'BARC.uk', 'BATS.uk', 'BP.uk', 'GSK.uk', 'HSBA.uk', 'TSCO.uk', 'BABA.us', 'NFLX.us', 'TSLA.us', 'GLEN.uk', 'RDSB.uk', 'BYND.us', 'UBER.us', 'ZM.us', 'JD.us', 'PDD.us', 'TME.us', 'WB.us', 'BILI.us', 'NVDA.us']
 
 fin_symbols = [
     'MOO', 
@@ -91,4 +47,4 @@ seconds_per_candle = {'1': 60,
                      '30': 1800,
                      '60': 3600,
                       'D': 86400
-                     }
+}
