1d4eb5137f8b2ee36fb506f644b2e486635d8b36
rrrudolph <rudysemail@gmail.com>
2021-03-16 12:40:54 -0500

    simplified and improved

--- correlations.py
+++ correlations.py
@@ -24,36 +24,26 @@ HIST_CANDLES = 11520  # 40 days
 
 # Settings for each time horizon 
 settings = {
-    'LTF': {
-        'NUM_CANDLES': HIST_CANDLES,  # once the historical data is filled out this should be edited for efficiency
-        'CORR_PERIOD': 864,  # 3 days
-        'MIN_CORR': abs(0.7),
-        'LOOKBACK_1': 1,  # 5 min
-        'LOOKBACK_2': 3,  # 15 min
-        'LOOKBACK_3': 6,  # 30 min
-    },
+    # 'LTF': {
+    #     'NUM_CANDLES': HIST_CANDLES,  # once the historical data is filled out this should be edited for efficiency
+    #     'CORR_PERIOD': 864,  # 3 days
+    #     'MIN_CORR': abs(0.6),
+    # },
     'MTF': {
         'NUM_CANDLES': HIST_CANDLES,
         'CORR_PERIOD': 1440,  # 5 days
-        'MIN_CORR': abs(0.65),
-        'LOOKBACK_1': 12,  # 1 hour
-        'LOOKBACK_2': 72,  # 6 hours
-        'LOOKBACK_3': 144, # 12 hours
+        'MIN_CORR': abs(0.60),
+        'SHIFT': 30,
     },
     'HTF': {
         'NUM_CANDLES': HIST_CANDLES,
         'CORR_PERIOD': 5760,  # 20 days
-        'MIN_CORR': abs(0.65),
-        'LOOKBACK_1': 288,   # 1 day
-        'LOOKBACK_2': 720,   # 2.5 days
-        'LOOKBACK_3': 1440,  # 5 days
+        'MIN_CORR': abs(0.60),
+        'SHIFT': 300,
     },
 # '4': {
 #     CORR_PERIOD: 4608,  # 16 days
 #     MIN_CORR: abs(0.5),
-#     LOOKBACK_1: 864,   # 3 days 
-#     LOOKBACK_2: 1152,  # 4 days
-#     LOOKBACK_3: 1440,  # 5 days
 #     },
 }
 
@@ -61,12 +51,6 @@ settings = {
 OHLC_CON = sqlite3.connect(ohlc_db)
 CORR_CON = sqlite3.connect(correlation_db)
 
-# bleh
-LOOKBACK_1 = 0
-LOOKBACK_2 = 0
-LOOKBACK_3 = 0
-key_symbol = 0
-cor_symbol = 0
 final_df = pd.DataFrame()
 
 def _get_data(symbol):
@@ -92,19 +76,6 @@ def _normalize(df):
 
     return df
 
-def _diffs_over_lookbacks(df, row, LOOKBACK_1=LOOKBACK_1, LOOKBACK_2=LOOKBACK_2, LOOKBACK_3=LOOKBACK_3):
-    ''' Returns the sum of diffs over the look backs. '''
-
-    i = df.loc[row, 'range_index']
-    row1 = df.index[df.range_index == i - LOOKBACK_1]
-    row2 = df.index[df.range_index == i - LOOKBACK_2]
-    row3 = df.index[df.range_index == i - LOOKBACK_3]
-
-    diff = (df.loc[row, 'close'] - df.loc[row1, 'close']).values[0]
-    diff += (df.loc[row, 'close'] - df.loc[row2, 'close']).values[0]
-    diff += (df.loc[row, 'close'] - df.loc[row3, 'close']).values[0]
-
-    return diff
 
 def _make_spread(spread):
     ''' These are all currently coming from MT5 so I will wrap a few
@@ -127,12 +98,12 @@ def _make_spread(spread):
 
     return spread_df
 
-def _append_result_to_final_df(row, key_diffs, cor_diffs, cor_value, key_symbol=key_symbol, cor_symbol=cor_symbol, df=final_df):
-    ''' Save any correlation data found before continuing to the next symbol. '''
-
-    df.loc[row, f'*{key_symbol}*'] = round(key_diffs, 3)
-    df.loc[row, f'{cor_symbol}'] = round(cor_diffs, 3)
-    df.loc[row, f'{cor_symbol}_corr'] = round(cor_value, 3)
+def _append_result_to_final_df(cor_rows, temp_key_df, cor_df, cor_values, key_symbol, cor_symbol, final_df):
+    ''' Save close prices and corr value for any correlated pair found before continuing to the next symbol. '''
+    for i in cor_rows:
+        final_df.loc[i, f'*{key_symbol}*'] = temp_key_df.loc[i, 'close']
+        final_df.loc[i, f'{cor_symbol}'] = cor_df.loc[i, 'close']
+        final_df.loc[i, f'{cor_symbol}_corr'] = round(cor_values.loc[i], 3)
 
 def find_correlations():
     ''' Find correlation between FX majors and other symbols and spreads
@@ -151,16 +122,14 @@ def find_correlations():
     for tf in settings:
         CORR_PERIOD = settings[tf]['CORR_PERIOD']
         MIN_CORR = settings[tf]['MIN_CORR']
-        LOOKBACK_1 = settings[tf]['LOOKBACK_1']
-        LOOKBACK_2 = settings[tf]['LOOKBACK_2']
-        LOOKBACK_3 = settings[tf]['LOOKBACK_3']
+        SHIFT = settings[tf]['SHIFT']
 
         length = len(mt5_symbols['majors']) + 1
         # Iter the fx majors
         for key_symbol in mt5_symbols['majors']:
             final_df = pd.DataFrame()
             length -= 1
-            print(f'{length} symbols left')
+            print(f'{tf}: {length} symbols left')
 
             # Get the data
             key_df = _get_data(key_symbol)
@@ -174,54 +143,41 @@ def find_correlations():
                 elif cor_symbol in mt5_symbols['others']:
                     cor_df = _get_data(cor_symbol)
 
-                # Ensure matching dfs
+                # Ensure matching df lengths
                 temp_key_df = key_df[key_df.index.isin(cor_df.index)].copy() # to avoid warnings
                 cor_df = cor_df[cor_df.index.isin(temp_key_df.index)]
 
                 temp_key_df = _normalize(temp_key_df)
                 cor_df = _normalize(cor_df)
 
-
-                # I only want to find symbols that lead the majors so I will shift the majors back
-                cor_values = temp_key_df.close.shift(-7).rolling(CORR_PERIOD).corr(cor_df.close)
+                # I only want to find symbols that lead the majors, so while scanning I will shift
+                # the majors back to see if a move correlates with another symbol which would have
+                # already made that move n rows prior
+                cor_values = temp_key_df.close.shift(-SHIFT).rolling(CORR_PERIOD).corr(cor_df.close)
 
                 # Check for randomness by shifting the other way and comparing the 2 shifts.
-                # If the shift back is not greater then this symbol should be skipped
-                cor_values_lagging = temp_key_df.close.shift(7).rolling(CORR_PERIOD).corr(cor_df.close)
-                if abs(cor_values.sum()) > abs(cor_values_lagging).sum():
-                    print(f'KEY:{key_symbol} CORR:{cor_symbol}')
+                # If the first shift's corr is not greater then its probably not a good line
+                cor_values_lagging = temp_key_df.close.shift(SHIFT).rolling(CORR_PERIOD).corr(cor_df.close)
+                # if abs(cor_values.sum()) > abs(cor_values_lagging).sum():
+                #     print(f'KEY:{key_symbol} CORR:{cor_symbol}')
 
                 # Get list of rows where correlation is above threshold
                 cor_rows = cor_values[cor_values > abs(MIN_CORR)].index.tolist()
-                # print('cor count:',cor_rows)
+                # if len(cor_rows) > 0:
+                    # print(f'cor count for {cor_symbol}:',len(cor_rows))
 
-                # Create range index columns to use with integer LOOKBACKs
-                temp_key_df['range_index'] = range(0, len(temp_key_df))
-                cor_df['range_index'] = range(0, len(cor_df))
-
-                # # Compare the normalized price differences between the key
-                # # and cor symbols.  Im saving the initial correlation
-                # # value to use later as a coefficient in deriving a buy/sell rating.
-                for row in cor_rows:
-
-                    if temp_key_df.loc[row, 'range_index'] < LOOKBACK_3:
-                        continue
-
-                    key_diffs = _diffs_over_lookbacks(temp_key_df, row)
-                    cor_diffs = _diffs_over_lookbacks(cor_df, row)
-
-                    # Get the new correlation values
-                    cor_value = cor_values.loc[row]
-
-                    # Save the sum of the diffs and correlation values
-                    _append_result_to_final_df(row, key_diffs, cor_diffs, cor_value)
-                    
+                _append_result_to_final_df(cor_rows, temp_key_df, cor_df, cor_values, key_symbol, cor_symbol, final_df)
+                
             # Once all the symbols and spreads have been analyzed, save the data
             # before continuing on to the next FX major symbol
-            final_df.to_sql(f'{key_symbol}_{tf}', CORR_CON, if_exists='replace', index=True)
-
+            print(f'{key_symbol} final df length:', len(final_df))
+            if len (final_df) > 2:
+                final_df.to_sql(f'{key_symbol}_{tf}', CORR_CON, if_exists='replace', index=True)
+            
+    OHLC_CON.close()
+    CORR_CON.close()
 
 print('\ntime started:', datetime.now())
 s = time.time()
 find_correlations()
-print('minutes elapsed:', time.time() - s)
+print('minutes elapsed:', (time.time() - s)/60)
