a94e60984ddb5c8f82b152a173f59855080b3571
rrrudolph <rudysemail@gmail.com>
2021-05-25 05:37:58 -0500

    changed periods

--- derive_value_from_correlations.py
+++ derive_value_from_correlations.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import sqlite3
 import pathlib
-from datetime import datetime
 import mplfinance as mpf
 import time
+import tqdm
 from create_db import value_db, correlation_db, econ_db, ohlc_db
 from corr_value_scanner import _normalize
 from symbols_lists import mt5_symbols, mt5_timeframes, indexes
@@ -18,7 +18,6 @@ ECON_CON = sqlite3.connect(r'C:\Users\ru\forex\db\economic_data.db')
 CORR_CON = sqlite3.connect(correlation_db)
 INDEXES_PATH = r'C:\Users\ru\forex\db\indexes'
 
-
 ''' 
 The goal of this module is to read the raw correlation data and derive buy/sell values from it.
 I'll do this by pair and also by index.  So once I have the data for each pair, I'll then
@@ -30,19 +29,21 @@ all those values row by row.
 '''
 
 periods = [
-    # '_LTF',
-    '_MTF',
-    '_HTF',
+    # 'LTF',
+    'MTF',
+    'HTF',
 ]
 
 
-def _add_to_df(df, pair, period, cor_df, cor_sum):
-    
+def _add_pairs_corr_values_to_aggregate_df(df, pair, period, cor_df, cor_sum) -> pd.DataFrame:
+    ''' The final utility function used at the end of calculating value
+    from the correlations data. '''
+
     df[pair] = cor_df[f'*{pair}*']
-    df[f'{period[1:]}'] = round(cor_sum, 4)
+    df[f'{period}'] = round(cor_sum, 4)
 
     df = _normalize(df, pair)
-    df = _normalize(df, period[1:])
+    df = _normalize(df, period)
 
     return df
 
@@ -52,15 +53,9 @@ def calculate_value_line_from_correlations() -> None:
 
     # keep the same format as the correlation scanner by itering periods and then pairs
     for period in periods:
-    
-    # I don't know how to get a list of table names from the db so 
-    # iter each pair and try to open a table from each timeframe
         for pair in mt5_symbols['majors']:
-            
-            name = pair + period
-            # This will be the final, single column df that is saved
-            df = pd.DataFrame()
-
+            name = pair + '_' + period
+            df = pd.DataFrame() # final df that is saved
             try:
                 cor_df = pd.read_sql(f'SELECT * FROM {name}', CORR_CON)
             except:
@@ -68,13 +63,11 @@ def calculate_value_line_from_correlations() -> None:
                 continue
             
             if len(cor_df) < 10:
-                print(f'cor_df didnt have any data for {name}')
+                print(f"There's only {len(cor_df)} rows of corr data for {name}. Skipping")
                 continue
             
-            # print(cor_df)
             cor_df.index = pd.to_datetime(cor_df.datetime)
             cor_df = cor_df.dropna(subset=[f'*{pair}*'])
-            # print(cor_df)
 
             # Parse the column names because I need to multiply each symbols column by its corr column
             # I left myself some keys so I could easily grab what I need
@@ -103,23 +96,23 @@ def calculate_value_line_from_correlations() -> None:
             cor_sum = cor_sum[cor_sum.notna()]
 
             # Save the normalized values of the pairs close prices and the derived value
-            df = _add_to_df(df, pair, period, cor_df, cor_sum)
+            df = _add_pairs_corr_values_to_aggregate_df(df, pair, period, cor_df, cor_sum)
             df.to_sql(name, VAL_CON, if_exists='replace', index=True)
 
 
 def value_line_rating_scanner() -> pd.DataFrame:
     ''' Check the correlation of price to a symbol's value line. That way
-    good value lines can be identified and focused on for trading signals. 
-    Remember to call calculate_value... to get the latest data first.'''
+    good value lines can be identified and focused on for trading signals.'''
 
     # This will save the symbol names and their value line's correlation score
     corr_values = pd.DataFrame()
     for symbol in mt5_symbols['majors']:  
         for period in periods:
-            name = symbol + period
+            name = symbol + '_' + period
             try:
                 df = pd.read_sql(f'SELECT * FROM {name}', VAL_CON)
-                corr_value = df[symbol].corr(df[period[1:]])
+                print(df.head())
+                corr_value = df[symbol].corr(df[period])
                 corr_values.loc[symbol, period] = corr_value
             except:
                 print(f"Couldn't open {name} from the value DB.")
@@ -128,47 +121,89 @@ def value_line_rating_scanner() -> pd.DataFrame:
     # Create an average corr column based on whatever period is notna(). row wise.
     non_nans = corr_values.notna().sum(axis=1)
     temp_df = corr_values.fillna(0)
-    corr_values['value'] = temp_df.sum(axis=1) / non_nans
+    corr_values['avg_value'] = temp_df.sum(axis=1) / non_nans
 
-    # find some details about the corr values by currency
-    corr_values = corr_values.sort_values(by=['value'], ascending=False)
-    highscores = corr_values[corr_values > 0.8]
-    print('\n ~~~ Correlation values ~~~ \n', highscores)
+    # Iter thru the correlation scores and save to a dict
+    corr_values = corr_values.sort_values(by=['HTF'], ascending=False)
+    # actually screw the averages, since HTF is always highest just roll with that. 
+    # it looks too high (0.95) for AUD&GBPJPY but whatever https://prnt.sc/13aj2f7
+    corr_dict = {}
+    for i in corr_values.index:
+        corr_dict[f'{i}_HTF'] = corr_values.loc[i, 'HTF']
 
-    return corr_values
+    return corr_dict
 
 
-def _make_single_value_line(value_data: dict) -> pd.Series:
-    ''' Make an average line of the various periods value '''
-    avg = pd.DataFrame()
-    for period in value_data:
-        avg[period] = value_data[period][period[-3:]]
+def _make_average_value_line(value_data) -> pd.Series:
+    ''' Make an average line of the various periods value. This 
+    is called by two separate functions, one passes a dict and 
+    the other a series. I need the same result in both cases 
+    but need to handle the data types passed accordingly. '''
 
-    non_nans = avg.notna().sum(axis=1)
-    temp_df = avg.fillna(0)
-    avg['value'] = temp_df.sum(axis=1) / non_nans
+    avg = pd.DataFrame() # used by both
+
+    if isinstance(value_data, dict):
+        for table_name, value in value_data.items():
+            avg[table_name] = value[table_name[-3:]]
+        # avg has columns "EURUSD_MTF", "EURUSD_HTF" with datetime index
+        non_nans = avg.notna().sum(axis=1)
+        temp_df = avg.fillna(0)
+
+    if isinstance(value_data, pd.DataFrame):
+        non_nans = value_data.notna().sum(axis=1)
+        temp_df = value_data.fillna(0)
+    
+    try:
+        avg['value'] = temp_df.sum(axis=1) / non_nans
+    except Exception as e:
+        print(e)
+        print('Tried averaging value lines but probably had nans in all columns.')
+        quit()
+
     return avg['value']
 
-def _make_value_line(symbol:str, period:list) -> pd.Series:
+def _make_value_line(symbol: str, average_line: bool=True) -> pd.Series:
     ''' Read the value DB for all available periods of the given symbol and return them as a df.
-    If return_single_line is True, create an average of the two lines and return a Series. '''
+    If return_average_line is True, create an average of the two lines and return a Series. '''
 
-    value_data = {}   # type: dict[symbol_name: pd.DataFrame]
+    internal_symbol = symbol
+    value_data = {}   # type: dict[symbol: pd.DataFrame]
     for period in periods:
-        # If the table exists open it
-        table_name = symbol + period
+        # If a symbol is passed with its period in the name just use that
+        if ('_LTF' or '_MTF' or '_HTF') in symbol:
+            print(111111111111)
+            table_name = symbol
+            # period = symbol[-3:]
+            internal_symbol = symbol[:6]
+            average_line = False
+        else:
+            table_name = symbol + '_' + period
+            print(222222222222222222)
+
         try:
             df = pd.read_sql(f'SELECT * FROM {table_name}', VAL_CON)
-            # structure is index, datetime, symbol normalized close, normalized value line
+            # structure is index, datetime, symbols normalized close (name=symbol), value lines normalized close (name=period)
             df.index = pd.to_datetime(df.datetime)
             df = df.drop(columns=['datetime'])
-            value_data[table_name] = df
-        except Exception:
+            df = df.dropna()
+
+        except Exception as e:
+            print('\n', table_name, 'got an error inside _make_value_line():')
+            print(e)
             continue
 
-    # In the future of I want to return all the value lines I can just omit this last function call
-    return _make_single_value_line(value_data)
+        # Check for minimum correlation score
+        value_corr = df[internal_symbol].corr(df[period])
+        if value_corr < 0.7:
+            print(f"{table_name}'s value line had a correlation of {round(value_corr, 2)}. Skipping.")
+            continue
 
+        value_data[table_name] = df
+
+    if average_line:
+        return _make_average_value_line(value_data)
+    
+    return df[period] # the value column only
 
 
 def _set_forecasts_as_volume(ohlc_df, index, timeframe) -> pd.DataFrame:
@@ -204,27 +239,20 @@ def _save_chart_pic(ohlc_df: pd.DataFrame, symbol: str, timeframe: str, overlay)
     
     if overlay is not None: 
         mpf.plot(ohlc_df, type='candle', tight_layout=True, 
-            show_nontrading=False, volume=True, title=f'{symbol}', 
-            addplot=overlay, savefig=f'{symbol}_{timeframe}.png')
+                show_nontrading=False, volume=True, title=f'{symbol}', 
+                addplot=overlay, savefig=f'{symbol}_{timeframe}.png')
     else:
         mpf.plot(ohlc_df, type='candle', tight_layout=True, 
-            show_nontrading=False, volume=True, title=f'{symbol}', 
-            savefig=f'{symbol}_{timeframe}.png')
+                show_nontrading=False, volume=True, title=f'{symbol}', 
+                savefig=f'{symbol}_{timeframe}.png')
 
+def _add_index_data_to_existing_df(existing: pd.Series, new_data: pd.Series) -> pd.Series:
+    ''' Two Series need to be combined into a df and their values averaged.
+    Nans are just overwritten by the single value that exists. If both are nans
+    then they are ffilled.'''
 
-def _add_data_to_existing(existing, new_data):
-    ''' When a period_data[period] of a different length gets added to what already exists,
-    nans will occur on any new index locations.  Those nans need to be overwritten by
-    the new values. However some of the period_data[period] has nans as well'''
-
-    col = existing.columns[0]
-    for row in new_data.itertuples(name=None, index=True):
-        i = row[0]
-        value = row[1]
-        if i in existing.index:
-            existing.loc[i, col] += value
-        else:
-            existing.loc[i,col] = value
+    temp = existing.to_frame(name='existing').join(new_data, how='outer')
+    temp = _make_average_value_line(temp)
 
     return existing
 
@@ -248,51 +276,46 @@ def _set_index_value() -> dict:
     }
     # Iter each index and add applicable pair values to it
     for symbol in mt5_symbols['majors']:
-        
         value_line = _make_value_line(symbol, periods)
-
-        # Add each period's data to its proper dict currency, inverting if necessary
+        # Add each period's data to its currency dict, inverting if necessary
         for index in indexes:
             if value_line.empty:
+                print("no value found for", symbol, '(_set_index_value)')
                 continue
             # Base currency
             if index == symbol[:3]:
                 if indexes[index].empty:
                     indexes[index] = value_line
                 else:
-                    _add_data_to_existing(indexes[index], value_line)
+                    _add_index_data_to_existing_df(indexes[index], value_line)
             # Counter currency
             elif index == symbol[-3:]:
                 inverted = value_line * -1  
                 if indexes[index].empty:
                     indexes[index] = inverted
                 else:
-                    _add_data_to_existing(indexes[index], inverted)
+                    _add_index_data_to_existing_df(indexes[index], inverted)
         
-        return indexes
+    return indexes
 
-def _create_index_value_line(ohlc_df: pd.DataFrame, value_line: pd.Series):
-    ''' Returns an mpf.addplot object '''
+def _make_mpf_addplot_overlay(ohlc_df: pd.DataFrame, value_line: pd.Series):
+    ''' Returns an mpf.make_addplot object '''
     
     # Add the value line series to the ohlc_df to ensure matching lengths
     ohlc_df = ohlc_df.join(value_line, how='outer')
-    print(ohlc_df)
-    ohlc_df.value = ohlc_df.value.fillna(method='ffill').fillna(method='bfill')
-    ohlc_df = ohlc_df.dropna(subset=['open'])
-    return mpf.make_addplot(ohlc_df.value, color='g', panel=0, secondary_y=True, width=1)
-    
-    # # Create temp df to ensure index alignment of value line and dataframe
-    # temp = ohlc_df.copy()
-    # temp['value'] = value_line
-    # temp.value.fillna(method='ffill').fillna(method='bfill')
-    # # temp['value'] = value_line[value_line.index.isin(ohlc_df.index)]
-    # # temp.value = temp.value.fillna(method='ffill').fillna(method='bfill')
-    # return mpf.make_addplot(temp.value, color='g', panel=0, secondary_y=True)
 
-def plot_charts(symbols:list, timeframe:str, num_candles:int=200, num_charts:int=10, include_forecasts:bool=True):
+    # Get name of last column (varies based on the caller)
+    value_col = ohlc_df.columns[-1]
+    ohlc_df[value_col] = ohlc_df[value_col].fillna(method='ffill').fillna(method='bfill')
+    ohlc_df = ohlc_df.dropna(subset=['open'])
+    return mpf.make_addplot(ohlc_df[value_col], color='g', panel=0, secondary_y=True,)
+    
+def plot_charts(symbols:list, timeframe:str, num_candles:int=200, include_forecasts:bool=True):
     ''' Get ohlc data from MT5 or indexes, overlay the value data and plot '''
 
-    value_lines = _set_index_value()
+    if symbols[0] in indexes:
+        value_lines = _set_index_value()
+
     for symbol in symbols:
         if symbol in indexes:
             # ohlc = pd.read_parquet(INDEXES_PATH,f'{index}_M5.parquet')
@@ -301,23 +324,39 @@ def plot_charts(symbols:list, timeframe:str, num_candles:int=200, num_charts:int
             ohlc = _resample(ohlc, timeframe)
             if include_forecasts:
                 ohlc = _set_forecasts_as_volume(ohlc, symbol, timeframe)
-            overlay = _create_index_value_line(ohlc, value_lines[symbol])
+            overlay = _make_mpf_addplot_overlay(ohlc, value_lines[symbol])
             try:
                 _save_chart_pic(ohlc, symbol, timeframe, overlay)
             except Exception as e:
                 print(e)
                 print('\n misaligned overlay with df probably. saving pics without value line.')
-            _save_chart_pic(ohlc, symbol, timeframe, [])
+                _save_chart_pic(ohlc, symbol, timeframe, [])
         
-        else:
-            ohlc_df = mt5_ohlc_request(symbol, mt5_timeframes[timeframe], num_candles=num_candles)
-            value_line = _make_value_line(symbol, periods)   
-            ohlc_df = _resample(ohlc_df, timeframe)
-            
-            # Make plot overlay
-            data = value_line[value_line.index.isin(ohlc_df.index)]
-            overlay = mpf.make_addplot(data, color='g', panel=0, secondary_y=True)
-            _save_chart_pic(ohlc_df, symbol, timeframe, overlay)
+        # Handle single symbols passed with or without their period tag 
+        if symbol[:6] or symbol in mt5_symbols['majors']:
+            if '_LTF' or '_MTF' or '_HTF' in symbol:
+                ohlc = mt5_ohlc_request(symbol[:6], mt5_timeframes[timeframe], num_candles=num_candles)
+            else:
+                ohlc = mt5_ohlc_request(symbol, mt5_timeframes[timeframe], num_candles=num_candles)
+            value_line = _make_value_line(symbol)   
+            ohlc = _resample(ohlc, timeframe)
+            overlay = _make_mpf_addplot_overlay(ohlc, value_line)
+            _save_chart_pic(ohlc, symbol, timeframe, overlay)
 
 
-plot_charts(indexes, 'H8', include_forecasts=True, num_candles=400)
+if __name__ == '__main__':
+
+    # Call this function to update the value database first
+    #calculate_value_line_from_correlations()
+    plot_charts(indexes, 'H8', include_forecasts=True, num_candles=400)
+
+    # Call this function to get the n pairs with the best value lines
+    # value_line_scores = value_line_rating_scanner()
+    # top_pairs = [k for (k,v) in value_line_scores.items() if v > 0.8]
+    # plot_charts(top_pairs, 'H4', include_forecasts=True, num_candles=100)
+    # print(top_n_pairs)
+
+# I need value_line_rating_scanner to return a dict with pair_peroid
+# as the key and the correlation value as the value. Then I can pass
+# the key in as the symbol name to plot charts and it will just give 
+# me that single line 
